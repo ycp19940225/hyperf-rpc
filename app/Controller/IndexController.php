3d86ba334f04d3685444008a3872746e567b08ce
ycp <820363773@qq.com>
2023-01-31 09:21:19 +0800

    Initial commit

---
+++ app/Controller/IndexController.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Controller;
+
+use Hyperf\HttpServer\Annotation\AutoController;
+use Hyperf\HttpServer\Annotation\RequestMapping;
+
+/**
+ * @AutoController()
+ * @package App\Controller
+ */
+class IndexController extends AbstractController
+{
+
+    public function index()
+    {
+        $user = $this->request->input('user', 'Hyperf');
+        $method = $this->request->getMethod();
+
+        return [
+            'method' => $method,
+            'message' => "Hello {$user}.",
+        ];
+    }
+
+    public function test()
+    {
+        return [
+            'method' => 'sss',
+        ];
+    }
+}
